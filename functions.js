d1b219bef47fffc02e905406a82a447b998cd018
Joe Corcoran <90690530+J-Corcoran@users.noreply.github.com>
2021-12-21 16:20:34 +0000

    media query added

--- functions.js
+++ functions.js
@@ -1,5 +1,12 @@
 var arrayId = [1, 2, 3];
 
+var input = document.getElementById('myInputBox');
+input.addEventListener("keyup", function(event){
+    if (event.keycode === 13){
+        document.getElementById("myBtn").click();
+    }
+})
+
 let addToDo = () =>{
     const item = document.querySelector('input').value;
     const myList = document.getElementById('toDoList');
